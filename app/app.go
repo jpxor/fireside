5ffa487580f79d8f5e68abca1e736f21ae49c70e
Josh <jsimonot@live.ca>
2023-12-05 02:09:51 -0500

    move parse & serialize into their own module
    The 'PTA' PlainTextAccounting module to provide
    struct types, parsing, serializing.
    
    all tests pass

--- app/app.go
+++ app/app.go
@@ -1,9 +1,20 @@
 package app
 
 import (
+	"fireside/pkg/pta"
 	"fmt"
 )
 
 func Run() {
+
 	fmt.Println("Fireside")
+	_, txs, err := pta.ParseJournal("pkg/pta/test/bench1.journal")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, tx := range txs {
+		fmt.Println(pta.WriteTransaction(tx))
+	}
 }
